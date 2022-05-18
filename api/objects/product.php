c1e7e4ddb31e6f3e34fa80d4811bdbc316ef836d
Best-Verie <irabestverie2016@gmail.com>
2022-05-18 08:53:00 +0200

    fet: read products

--- api/objects/product.php
+++ api/objects/product.php
@@ -15,5 +15,22 @@ class Product {
     public function __construct($db) {
         $this->conn = $db;
     }
+
+    function read(){
+        $query = "SELECT
+                    c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
+                FROM
+                    " . $this->table_name . " p
+                    LEFT JOIN
+                        categories c
+                            ON p.category_id = c.id
+                ORDER BY
+                    p.created DESC";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+
+        return $stmt;
+    }
 }
 ?>
